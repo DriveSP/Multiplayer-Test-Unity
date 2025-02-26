3164a759c5d5c13b1b4c30007626eb8b4e51b093
DriveSP <gtavvice@hotmail.es>
2025-02-26 18:34:03 +0100

    ListPlayers
    ListPlayers

--- Assets/Scripts/TestLobby.cs
+++ Assets/Scripts/TestLobby.cs
@@ -10,6 +10,7 @@ public class TestLobby : MonoBehaviour
 {
     private Lobby hostLobby;
     private float heartBeatTimer;
+    private string playerName;
 
     private async void Start()
     {
@@ -17,10 +18,12 @@ public class TestLobby : MonoBehaviour
 
         AuthenticationService.Instance.SignedIn += () =>
         {
-            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+            SmartConsole.Log("Signed in " + AuthenticationService.Instance.PlayerId);
         };
-
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        playerName = "Jairo" + UnityEngine.Random.Range(10, 999);
+        SmartConsole.Log("Your player name is: "+playerName);
     }
 
     private void Update()
@@ -52,10 +55,16 @@ public class TestLobby : MonoBehaviour
         {
             string lobbyName = "LobbyName";
             int maxPlayers = 4;
-            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions
+            {
+                IsPrivate = false,
+                Player = GetPlayers()
+            };
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, createLobbyOptions);
 
             hostLobby = lobby;
-            SmartConsole.Log("Create Lobby! " + lobbyName + " " + lobby.MaxPlayers);
+            SmartConsole.Log("Create Lobby! " + lobbyName + " " + lobby.MaxPlayers + " " + lobby.Id + " " + lobby.LobbyCode);
+            PrintPlayers(hostLobby);
         }
         catch (LobbyServiceException e)
         {
@@ -100,17 +109,81 @@ public class TestLobby : MonoBehaviour
     private async void JoinServer()
     {
         try
-        {        
+        {
             QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
-
+            
             await LobbyService.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
-
             SmartConsole.Log("Lobby joined");
         }
         catch (LobbyServiceException e)
         {
             Debug.Log(e);
         }
+    }
+
+    [Command]
+    private async void JoinServerCode(string lobbyCode)
+    {
+        try
+        {
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+
+            await LobbyService.Instance.JoinLobbyByCodeAsync(lobbyCode);
+
+            SmartConsole.Log("Lobby joined with code: " + lobbyCode);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+
+    private async void QuickJoinServer()
+    {
+        try
+        {
+            QuickJoinLobbyOptions quickJoinLobbyOptions = new QuickJoinLobbyOptions
+            {
+                Player = GetPlayers()
+            };
+            Lobby lobbyJoined = await LobbyService.Instance.QuickJoinLobbyAsync(quickJoinLobbyOptions);
+            SmartConsole.Log("Joined quickly");
+
+            PrintPlayers(lobbyJoined);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private void PrintPlayers(Lobby lobby)
+    {
+        try
+        {
+            SmartConsole.Log("Players in lobby: " + lobby.Name);
+            foreach (Player player in lobby.Players)
+            {
+                SmartConsole.Log("Player: " + player.Id + " Name: " + player.Data["PlayerName"].Value);
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    private Player GetPlayers()
+    {
+        return new Player(AuthenticationService.Instance.PlayerId)
+        {
+            Data = new Dictionary<string, PlayerDataObject>
+                    {
+                        {"PlayerName", new PlayerDataObject (PlayerDataObject.VisibilityOptions.Member, playerName) }
+                    }
+        };
     }
 
 }
